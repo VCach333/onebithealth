f071a3ff89b605aa87dd2d35d1a4a225924eeacb
cachsoftwares <cachsoftwares@gmail.com>
2024-04-19 19:27:02 +0100

    calculo funcionando

---
+++ src/components/Form/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import ResultImc from '../ResultImc/'
+
+export default function Form() {
+
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [messageImc, setMessageImc] = useState('preencha o peso e a altura');
+    const [imc, setImc] = useState(null);
+    const [textButton, setTextButton] = useState('Calcular');
+
+    function imcCalculator() {
+        return setImc((weight / (height * height)).toFixed(2));
+    }
+
+    function validationImc() {
+        if(weight != null && height != null) {
+            imcCalculator();
+            setHeight(null);
+            setWeight(null);
+            setMessageImc('Seu IMC é igual:');
+            setTextButton('Calcular Novemente');
+            return;
+        }
+
+        setImc(null);
+        setTextButton('Calcular');
+        setMessageImc('preencha o peso e a altura');
+    }
+
+    return(
+        <View>
+            <View>
+                <Text>Altura (m)</Text>
+                <TextInput
+                    onChangeText={setHeight}
+                    value={height}
+                    keyboardType='numeric'
+                    placeholder='ex.: 1.75'
+                    />
+                
+                <Text>Peso (kg)</Text>
+                <TextInput
+                    onChangeText={setWeight}
+                    value={weight}
+                    placeholder='ex.: 65'
+                    keyboardType='numeric'
+                />
+
+                <Button 
+                    title={textButton}
+                    onPress={() => {validationImc()}}
+                />
+            </View>
+
+            <ResultImc messageResultImc={messageImc} resultImc={imc} />
+        </View>
+    );
+}
